1419f38c3fd8c0e28ac08d8b3e577b8bddc5b730
fahe94 <fabian.herrmann@tum.de>
2024-12-05 14:04:33 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
 // script.js
-document.getElementById("submit-button").addEventListener("click", function () {
-  const correctPassword = "1843"; // Ersetze durch dein Passwort
+document.getElementById("submit-button").addEventListener("click", function (function(event) {
+      event.preventDefault(); 
+  const password = document.getElementById("password").value;
+  //Passwörter und Zielseiten
+  const routes={
+    "1234": "seite1.html",
+    "1235": "seite2.html",
+    "1236": "seite3.html",
+    "1237": "seite4.html",
+  };
+        // Überprüfen, ob das Passwort existiert
+      if (routes[password]) {
+        window.location.href = routes[password]; // Weiterleitung zur entsprechenden Seite
+      } else {
+        alert("Falsches Passwort!"); // Fehlernachricht
+      }  
+  
+ /* const correctPassword = "1843"; // Ersetze durch dein Passwort
   const enteredPassword = document.getElementById("password-input").value;
 
   if (enteredPassword === correctPassword) {
@@ -12,5 +28,5 @@ document.getElementById("submit-button").addEventListener("click", function () {
     setTimeout(() => {
       errorMessage.style.display = "none"; // Fehlermeldung nach 2 Sekunden ausblenden
     }, 2000);
-  }
+  }*/
 });
